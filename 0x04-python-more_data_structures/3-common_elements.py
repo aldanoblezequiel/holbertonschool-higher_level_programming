06026bbab7ae7a5c6de942929638046bfa705a5a
aldanoblezequiel <aldabaldenoblezequiel@gmail.com>
2022-05-04 19:30:00 -0700

    Task 3 corrected

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,9 +1,4 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
-    ret = []
-    for n in set_1:
-        for i in set_2:
-            if n == j:
-                ret.append(i)
-                break
-    return ret
+
+    return (set_1 & set_2)
