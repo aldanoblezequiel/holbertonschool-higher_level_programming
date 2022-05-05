7cfb217ec0282d8b6c030cde3d34343af9a09b98
aldanoblezequiel <aldabaldenoblezequiel@gmail.com>
2022-05-04 19:51:03 -0700

    Task 103

---
+++ 0x04-python-more_data_structures/103-python.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <Python.h>
+/**
+ * print_python_list - Print list
+ * @p: Ptr to void?
+ * Return: Null
+ */
+void print_python_list(PyObject *p)
+{
+	void(PyObject)
+}
