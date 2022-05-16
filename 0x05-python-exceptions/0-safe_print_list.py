c9443bf3a04d70dc061f82c739bdeb0158e924dd
aldanoblezequiel <aldabaldenoblezequiel@gmail.com>
2022-05-16 16:52:41 -0700

    Task 0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    string = ""
+    large = 0
     i = 0
-    while (i < x)
-
+    try:
+        for i in my_list:
+            if (large < x):
+                print(i, end="")
+                large += 1
+        print()
+    finally:
+        return (large)
