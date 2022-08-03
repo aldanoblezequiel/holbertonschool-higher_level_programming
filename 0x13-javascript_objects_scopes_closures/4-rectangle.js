d6c0d91c82fd865081445e0d64a8c5466c746994
aldanoblezequiel <aldabaldenoblezequiel@gmail.com>
2022-08-02 19:24:31 -0700

    Tasks mandatory corrected

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,25 @@
+#!/usr/bin/node
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      [this.width, this.height] = [w, h];
+    }
+  }
+
+  print () {
+    for (let n = 0; n < this.height; n++) {
+      for (let i = 0; i < this.width; i++) process.stdout.write('X');
+      console.log('');
+    }
+  }
+
+  rotate () {
+    [this.width, this.height] = [this.height, this.width];
+  }
+
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
+};
+// el 10 armando
