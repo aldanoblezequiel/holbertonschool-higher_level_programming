bf45e01dcbff641147fbed9f44330434819675ed
aldanoblezequiel <aldabaldenoblezequiel@gmail.com>
2022-08-02 19:47:24 -0700

    sueltaaa

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,24 +1,7 @@
 #!/usr/bin/node
-const dict = require('./101-data.js').dict;
-const sorted = Object.keys(dict).map(function (key) {
-  return [key, dict[key]];
-});
-sorted.sort(function (first, second) {
-  return first[1] - second[1];
-});
-const newdict = {};
-let flag = sorted[0][1];
-let array = [];
-for (let item = 0; item < sorted.length; item++) {
-  if (sorted[item][1] === flag) {
-    array.push(sorted[item][0]);
-  } else {
-    newdict[flag] = array;
-    flag = sorted[item][1];
-    array = [];
-    item--;
-  }
-  newdict[flag] = array;
-}
-console.log(newdict);
+const dict = require('./101-data').dict;
+const sorted = {};
+Object.values(dict).forEach(function (index) { sorted[index] = []; });
+Object.keys(dict).forEach(key => sorted[dict[key]].push(key));
+console.log(sorted);
 // vpi yen
