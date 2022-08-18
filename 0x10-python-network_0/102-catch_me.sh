518f34f1323c99278dd9b7c9a1e793b774c26248
aldanoblezequiel <aldabaldenoblezequiel@gmail.com>
2022-08-18 12:08:38 -0700

    Task 8  corrected

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #Requesting 0000500 and causing server response messag
-curl -sL 0.0.0.0:5000/catch_me_3 -X PUT -H "Origin:You got me!"
+curl -sL 0.0.0.0:5000/catch_me_3 -X PUT -H "Origin:HolbertonSchool"
