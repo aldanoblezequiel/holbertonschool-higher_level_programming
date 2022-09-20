e1e93e20ac9055dc6a89d9119bded2f8221f0c1f
aldanoblezequiel <aldabaldenoblezequiel@gmail.com>
2022-09-19 18:40:46 -0700

    Task up to  9

---
+++ 0x15-javascript-web_jquery/8-script.js
@@ -0,0 +1,7 @@
+$(document).ready(function () {
+    $.getJSON("https://swapi-api.hbtn.io/api/films/?format=json", function (data) {
+        $.each(data.results, function (index, value) {
+            $("#list_movies").append("<li>" + value.title + "</li>");
+        });
+    });
+});
